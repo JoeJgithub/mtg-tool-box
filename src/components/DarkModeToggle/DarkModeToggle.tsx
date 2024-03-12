957b2346c99d505f7a67eb4ce0610e9b2fe3a2ad
Joe Jacobs <josephedwjacobs@gmail.com>
2024-03-12 21:00:36 +1300

    Added dark mode toggle button

---
+++ src/components/DarkModeToggle/DarkModeToggle.tsx
@@ -0,0 +1,18 @@
+import { ActionIcon, Button, Group, useMantineColorScheme } from '@mantine/core';
+import { MdDarkMode, MdOutlineDarkMode } from "react-icons/md";
+
+export function DarkModeToggle(props: {
+  size: string,
+}) {
+  const { colorScheme, setColorScheme } = useMantineColorScheme();
+
+  return (
+    <ActionIcon {...{props}}>
+      {colorScheme === 'light' ? (
+        <MdDarkMode onClick={() => setColorScheme('dark')} />
+      ) : (
+        <MdOutlineDarkMode onClick={() => setColorScheme('light')} />
+      )}
+    </ActionIcon>
+  );
+}

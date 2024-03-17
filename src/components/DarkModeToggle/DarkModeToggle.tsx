ce7dafcee2df6a7d02660d3094a86773c3c1531c
Joe Jacobs <josephedwjacobs@gmail.com>
2024-03-17 23:38:11 +1300

    prettier cleanup

--- src/components/DarkModeToggle/DarkModeToggle.tsx
+++ src/components/DarkModeToggle/DarkModeToggle.tsx
@@ -1,13 +1,11 @@
 import { ActionIcon, Button, Group, useMantineColorScheme } from '@mantine/core';
-import { MdDarkMode, MdOutlineDarkMode } from "react-icons/md";
+import { MdDarkMode, MdOutlineDarkMode } from 'react-icons/md';
 
-export function DarkModeToggle(props: {
-  size: string,
-}) {
+export function DarkModeToggle(props: { size: string }) {
   const { colorScheme, setColorScheme } = useMantineColorScheme();
 
   return (
-    <ActionIcon {...{props}}>
+    <ActionIcon {...{ props }}>
       {colorScheme === 'light' ? (
         <MdDarkMode onClick={() => setColorScheme('dark')} />
       ) : (
